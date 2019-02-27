f96584eb484d791e9eaaa8c31be4e70c32911ee4
locareen <ksmt4699@gmail.com>
2019-02-27 20:03:17 +0900

    単元入力時にファイル内容が表示されるように改良

--- index.sh
+++ index.sh
@@ -15,27 +15,40 @@ do
 	  read date
 	fi
 #日付入力は一度入力したら"n"を入力するまでスキップ
+
 	SKIP="ON"
 
 	echo "教科を入力"
 	echo "終了にはqを入力"
-	echo "日付変更にはnを入力"
+	echo "はじめへ戻るにはnを入力"
 	read sub
-	if [ "$sub" = "q" ]; then
-	  break
-	elif [ "$sub" = "n" ]; then
-	  SKIP=""
-	  continue 5
-	else
 
+	   if [ "$sub" = "q" ]; then
+	     break
+	   elif [ "$sub" = "n" ]; then
+	  	  SKIP=""
+				continue
+		 fi
+ 
 	  echo "単元を入力"
-	  read unit
+		echo "ファイル表示にはseeと入力"
+		flag="ON"
+		while [ "$flag" = ON ]
+		do
+			echo "単元を入力"
+	  	read unit
+			if [ "$unit" = "see" ]; then
+				cat "$path" | grep "$sub"
+				
+		
+			else
+				flag="OFF"
+			fi
+		done
 
 	  echo "その他備考など"
-	  read about
+		read about
 
-          echo "$date,$sub,$unit,#$about" >> "$path"
+	  echo "$date,$sub,$unit,#$about" >> "$path"
 #日付,教科,単元,#備考の形で出力
-	fi
 done
-
